b3991451eccd2d0dda7175967b706ce1233173ef
TekMike365 <miskosklenka@gmail.com>
2025-04-07 17:08:16 +0200

    Added Fabric template

---
+++ src/main/java/tekmike365/vivacore/VivaCore.java
@@ -0,0 +1,17 @@
+package tekmike365.vivacore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VivaCore implements ModInitializer {
+	public static final String MOD_ID = "viva-core";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
---
+++ src/main/java/tekmike365/vivacore/VivaCoreDataGenerator.java
@@ -0,0 +1,13 @@
+package tekmike365.vivacore;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VivaCoreDataGenerator implements DataGeneratorEntrypoint {
+
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+
+}
